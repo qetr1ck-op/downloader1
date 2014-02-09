54f822fec7c465e5cd8756aed4cadfcb1d9d2065
qetr1ck-op <orestprustayko@gmail.com>
2014-02-10 00:41:55 +0200

    PhoneGap.exac > cordova.exec

--- Downloader.android.js
+++ Downloader.android.js
@@ -1,7 +1,7 @@
 function Downloader() {}
 
 Downloader.prototype.preload = function(fileUrl, params, win, fail) {
-	PhoneGap.exec(win, fail, "Downloader", "preload", [fileUrl, params]);
+	cordova.exec(win, fail, "Downloader", "preload", [fileUrl, params]);
 };
 
 Downloader.prototype.install = function() {   
